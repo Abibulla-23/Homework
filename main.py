80a66712526eaed40a52933f3adcac77be3856e3
Abibulla-23 <Abibamad@gmail.com>
2024-04-06 21:11:49 +0300

    Chsnges

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
-def main(input: str):
-    s = input('Введите запрос в формате: целое число от 1 до 10 включительно, затем через пробел знак действия(+,-,*,/), затем через пробел второе целое число от 1 до 10 включительно:')
+
+def main(s):
     se = s.split()
+    c = se[1]
     try:
         a = int(se[0])
         b = int(se[2])
+        if c == '+' or '-' or '*' or '/':
+            if c == '+' and 0 <= a <= 10 and 0 <= b <= 10:
+                f = str(a + b)
+            elif c == '-' and 0 <= a <= 10 and 0 <= b <= 10:
+                f = str(a - b)
+            elif c == '*' and 0 <= a <= 10 and 0 <= b <= 10:
+                f = str(a * b)
+            elif c == '/' and 0 <= a <= 10 and 0 <= b <= 10:
+                f = str(a // b)
+            return f
+
     except:
-        print("Введите целое число!")
-        exit()
-    c = se[1]
-    try:
-        c == '+' or '-' or '*' or '/'
-    except:
-        print('Введите один из знаков действия(+,-,*,/)')
-        exit()
-    if 0<=a<=10 and 0<=b<=10:
-        if c == '+':
-            print(a+b)
-        elif c == '-':
-            print(a-b)
-        elif c == '*':
-            print(a*b)
-        elif c == '/':
-            print(a//b)
-    else:
-        print("Введите целые числа от 1 до 10 включительно!")
-main(input)
-main(input)
+        print("Введите корректный запрос!")
+
+
+assert main('12 + 1') == '13'
